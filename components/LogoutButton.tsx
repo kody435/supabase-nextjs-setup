b2b9974ce64b457edf628349d9457bed3851416c
Param <bittusmaths@gmail.com>
2023-08-13 21:54:34 +0530

    I'm not perfect, I'm flawed

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -1,7 +1,7 @@
 export default function LogoutButton() {
   return (
     <form action="/auth/sign-out" method="post">
-      <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
+      <button className="py-2 px-4 rounded-md no-underline bg-blue-500">
         Logout
       </button>
     </form>
