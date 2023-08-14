22ded481ea8f26677e5725f0e4bb77233dcbf5f9
Param <bittusmaths@gmail.com>
2023-08-14 22:04:11 +0530

    small push!

--- app/auth/role/page.js
+++ app/auth/role/page.js
@@ -2,6 +2,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
+export const dynamic = "force-dynamic";
+
 export default async function page() {
   const supabase = createServerComponentClient({ cookies });
 
