6eba699c04395586696e30cdb8577f2736a32fe6
Param <bittusmaths@gmail.com>
2023-08-15 09:42:18 +0530

    Push's to the proper location

--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -26,7 +26,7 @@ export async function POST(request: Request) {
     );
   }
 
-  return NextResponse.redirect(requestUrl.origin, {
+  return NextResponse.redirect(`${requestUrl.origin}/auth/role`, {
     // a 301 status is required to redirect from a POST to a GET route
     status: 301,
   });
