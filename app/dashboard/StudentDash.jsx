0ca6e4cdf45a43a997a5c417d04c93c104bffb8f
Param <bittusmaths@gmail.com>
2023-08-16 10:11:24 +0530

    Go go go go...

--- app/dashboard/StudentDash.jsx
+++ app/dashboard/StudentDash.jsx
@@ -3,9 +3,11 @@ import LogoutButton from "@/components/LogoutButton";
 export default function StudentDash({user}) {
   return (
     <div>
-      <div className="flex flex-row justify-between p-10 ">
+      <div className="flex flex-row justify-between p-10 items-center">
         <h3>Welcome {user.email}!</h3>
-        <LogoutButton />
+        <div className="bg-black text-white px-6 py-2.5 rounded-2xl">
+          <LogoutButton />
+        </div>
       </div>
 
       <div></div>
