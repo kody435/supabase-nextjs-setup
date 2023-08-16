ee8fb3244b2341a674c331054bd82d96fa47a39a
Param <bittusmaths@gmail.com>
2023-08-16 10:01:08 +0530

    Pushing hahaha, idk why logout button is not showing up

--- app/dashboard/StudentDash.jsx
+++ app/dashboard/StudentDash.jsx
@@ -3,7 +3,7 @@ import LogoutButton from "@/components/LogoutButton";
 export default function StudentDash({user}) {
   return (
     <div>
-      <div className="flex flex-row justify-between p-10">
+      <div className="flex flex-row justify-between p-10 ">
         <h3>Welcome {user.email}!</h3>
         <LogoutButton />
       </div>
