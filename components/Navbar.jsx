cbb088bf6489f80dbe1ed481c2a723de0c92aca4
Param <bittusmaths@gmail.com>
2023-08-16 15:13:41 +0530

    Do this much for now

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,18 @@
+"use client"
+
 import LogoutButton from "./LogoutButton";
+import { usePathname } from "next/navigation";
 
 export default function Navbar() {
+  const router = usePathname();
   return (
     <div className="flex flex-row justify-between items-center py-2 px-5 border">
       <h2 className="text-2xl flex justify-between items-center ">
         <span className="text-5xl font-extrabold">L</span>
         <span className="underline mt-3 font-normal">ucidian</span>
       </h2>
-      <LogoutButton />
+      
+      {router === "/login" || '/signup' ? (<div></div>) :(<LogoutButton />) }
     </div>
   );
 }
--- components/Student/UpdateComp.jsx
+++ components/Student/UpdateComp.jsx
@@ -4,8 +4,8 @@ export default function UpdateComp({ updates }) {
 
   return (
     <div className="border-x">
-      {updates.map((update, id) => (
-        <div key={id} className="border-y p-3">
+      {updates.map((update) => (
+        <div key={update.update_id} className="border-y p-3">
           <h3>{update.text}</h3>
         </div>
       ))}
